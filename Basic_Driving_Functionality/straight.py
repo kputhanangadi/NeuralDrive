43ccd2abd835b76e328fb4069af54349664183b1
Kevin Puthanangadi <117777515+kputhanangadi@users.noreply.github.com>
2023-09-14 23:42:17 -0700

    updated formatting

--- Basic_Driving_Functionality/straight.py
+++ Basic_Driving_Functionality/straight.py
@@ -1,6 +1,4 @@
-# first we import socketio we use this to connect to the PiCar
 import socketio
-# next we import time we will use this to delay the code
 import time
 
 # create a socketio object
@@ -11,8 +9,6 @@ sio = socketio.Client()
 
 # connect to the PiCar
 # connects using 192.168.0.10:3000
-# we try to connect if we fail we tell the user
-# that they probably are not connected to the PiCar network
 try:
     sio.connect('http://192.168.0.10:3000')
 
@@ -20,23 +16,28 @@ except:
     print("Failed to connect to PiCar")
     print("Check that your laptop is connected to the PiCar network")
 
-# now we can start moving the car
-# we set the angle of the steering to 90 degrees
+# start moving the car
+# set the angle of the steering to 90 degrees
 # this is the middle position (180 being fully right and 0 being fully left)
 sio.emit('steer', 97.1)
-# we set the speed to 50%
+
+# set the speed to 50%
 sio.emit('drive', 50)
-# we wait for 5 second this is how long the car will move for
+
+# wait for 5 second this is how long the car will move for
 time.sleep(5)
-# now we stop the car by setting the speed to 0%
+
+# stop the car by setting the speed to 0%
 sio.emit('drive', 0)
-# we stay stopped for 2 seconds
+
+# stay stopped for 2 seconds
 time.sleep(2)
-# now we reverse by setting the speed to -50%
+
+# reverse by setting the speed to -50%
 sio.emit('drive', -50)
-# we wait for 5 seconds again
+
+# wait for 5 seconds again
 time.sleep(5)
-# now we stop the car by setting the speed to 0%
-# don't forget to stop the car!
-# Otherwise it'll run away!
+
+# stop the car by setting the speed to 0%
 sio.emit('drive', 0)
